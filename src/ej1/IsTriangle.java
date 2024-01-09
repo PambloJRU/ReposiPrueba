925b2ac636f5fe0944dfd289a8463d0ecfb833ee
Pablo Ramirez <pablo18jru@gmail.com>
2024-01-09 16:53:39 -0600

    mi primer commit

---
+++ src/ej1/IsTriangle.java
@@ -0,0 +1,19 @@
+package ej1;
+
+public class IsTriangle {
+
+	public static boolean isATriangle(int SideA,int SideB,int SideC) {
+		
+		if(triangleVerifications(SideA,SideB,SideC))
+		return SideA+SideB > SideC && SideB+SideC > SideA && SideC+SideA > SideB;
+		else
+			return false;
+	}
+	
+	public static boolean triangleVerifications(int SideA,int SideB,int SideC) {
+		
+		return SideA >= 1 && SideB >= 1 && SideC >= 1;
+		
+	}
+	
+}
---
+++ src/ej1/Main.java
@@ -0,0 +1,11 @@
+package ej1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(IsTriangle.isATriangle(3, 1, 2));
+
+	}
+
+}
